42799d52dfc6e236967db282dafba4d3fd60a69c
Павел Рафеев <rafeev@office.bitrix.ru>
2017-06-20 18:06:29 +0200

    add footer

--- js/main.js
+++ js/main.js
@@ -1,35 +1,18 @@
 window.onload = function(){
 	
-	var bodyWidth = document.body.offsetWidth;
-
-	function setTwitterStyle() {
-		frameContent = document.getElementById('twitter-widget-0').contentWindow.document;
-		if (frameContent !== null) 
-		{
-			var frameBlockBg = frameContent.querySelector('.timeline-Widget');
-			frameBlockBg.style.background = 'transparent';
-		}
+	var showTab = function()
+	{
+		console.log('ro');
 	}
 
-	function slideMenu() {
+	var tabs = document.querySelectorAll('.adress-tabs__item');
 
-		var buttonToggle = document.querySelector('.show-menu');
-		var body = document.querySelector('.body');
-
-		buttonToggle.addEventListener('click', function(){
-			body.classList.toggle('left-menu-open');
-		}, false);
-
-		if(bodyWidth > 740)
-		{
-			body.classList.remove('left-menu-open');
-		}
-
+	for(var i =0; tabs.length < 0; i++)
+	{
+		tabs[i].addEventListener('click', showTab, false);
+		console.log(tabs[i]);
 	}
 
-	if(bodyWidth <= 740) {
-		slideMenu()
-	}
-
-	window.addEventListener('resize', slideMenu, false);
+	console.log(tabs);
+	
 }
